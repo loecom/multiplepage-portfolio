48afe3068190587401df8dc90b3ecfba8c5ec2ae
酒破儿 <154944277+loecom@users.noreply.github.com>
2025-10-18 20:34:34 +0800

    Update global.ts

--- src/config/global.ts
+++ src/config/global.ts
@@ -28,14 +28,11 @@ export const globalConfig = {
   },
   footer: {
     aria: "Footer Navigation",
-    copyright: (
-      <>
-        © 2025 我的回家之路 | 设计制作: 酒破儿
-        <a href='https://beian.miit.gov.cn/' target='_blank' rel='noopener noreferrer'>
-          浙ICP备2025203608号-1
-        </a>
-      </>
-    ),
+    copyright: "© 2025 我的回家之路 | 设计制作: 酒破儿",
+    link: {
+        text: "浙ICP备2025203608号-1",
+        href: "https://beian.miit.gov.cn/"
+    },
     social: {
       twitter: "https://x.com/loecom",
       github: "https://github.com/loecom",
